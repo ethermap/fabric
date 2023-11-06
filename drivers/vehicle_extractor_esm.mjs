f0fd125051cbe90f90ddbd3d43b5d809310eb223
Psy Tron <m@psytron.com>
2023-11-06 05:14:05 -0800

    parse features

--- drivers/vehicle_extractor_esm.mjs
+++ drivers/vehicle_extractor_esm.mjs
@@ -35,6 +35,57 @@ async function rawMaps(){
     })
 }
 
+function parseFeatures( features_in ){
+
+
+    var f = features_in;
+
+    var outlist = [];
+
+    for( var f in features_in ){
+        var obj_in = features_in[f];
+
+        var obj={}
+        obj.uuid=obj_in.properties.id;
+        obj.name = obj_in.properties.name;
+        
+        obj.geometry = obj_in.geometry.coordinates;
+        obj.brand = 'cablenet'
+        obj.label='coord';
+        if( obj_in.geometry.type == "MultiLineString" ){
+            var i=7;
+            obj.label='route';
+            obj.geometry = obj_in.geometry.coordinates;
+            obj.type=obj_in.geometry.type;
+            obj.coordinates = [0,0,0];   
+            obj.color = obj_in.properties.color;
+            var ui = 1;
+        }else{
+            obj.coordinates = obj_in.geometry.coordinates;   
+        }
+        outlist.push( obj );
+    }
+
+    return outlist; 
+    
+}
+
+async function features(){
+    console.log(' raw maps called in worke ')
+    return new Promise( ( resolve , reject )=>{
+
+        var promise1 = fetchWrapped( "/data/undersea_landing_points.json" )
+        var promise2 = fetchWrapped( "/data/undersea_cables.json" )
+        Promise.all([ promise1 , promise2  ]).then( (values) => {
+
+            var objects1 = parseFeatures( values[0]['features'] );
+            var objects2 = parseFeatures( values[1]['features'] );
+            var combinedObjects = [...objects1, ...objects2];
+            resolve( { nodes:combinedObjects , links:[] , meta:{ origin:'extractor' } } )
+        });
+    })
+}
+
 async function downloadBlob( obj ){
     console.log(' DLB ')
     return new Promise( ( resolve , reject )=>{
@@ -55,4 +106,4 @@ async function downloadBlob( obj ){
 
 
 
-export { rawMaps , downloadBlob }
+export { rawMaps , features,  downloadBlob }
